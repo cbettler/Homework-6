1c9a5db065de2b301d0fc075c430f7337977b69d
cbettler <carlee.bettler@gmail.com>
2016-04-26 14:04:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ void writeOutFile(int A[], int size);
 
 
 int main() {
-    string file1name = "everybodys_socials.txt" ;
+    string file1name = "/Users/carleebettler/Documents/Comp 271/Unit 3/everybodys_socials.txt" ;
     
     string line, line2;
     
@@ -64,7 +64,7 @@ int main() {
     
     file1.close();
     
-   
+    
     //I add the contents of my vector to a string array called SSoriginal of the vector's size
     string* SSoriginal = new string[SSNums.size()];
     
@@ -72,9 +72,15 @@ int main() {
         SSoriginal[i] = SSNums[i];
     }
     
-    //I am prompting the user for a number for quadratic probing
-    cout<<"Please enter a number between 0 and 45000000"<<endl;
+    //I am prompting the user for a number for quadratic probing. Note: This conditional statement freaks out if the user enters a value too big for the while statement to handle... it became infinite, which it didn't do when I changed the parameter for a smaller max number. It also doesn't have a condition for non numerical answers. So I'm assuming my user is smart enough to enter a number, and it is not a number if the billions
+    cout<<"Please enter a number between 0 and 450000000 for quadratic probing"<<endl;
     cin >> input;
+    
+    while(!(input > 0 && input < 450000000)) {
+        cout<<"Please enter a number between 0 and 450000000 for quadratic probing"<<endl;
+        cin >> input;
+    }
+    
     
     //Implementing my functions
     int* HashSlingingSlasher = extraction_method(SSoriginal, SSNums.size(), input);
@@ -114,7 +120,7 @@ int* extraction_method(string A[], int arraySize, int ProbeNum) {
         SSNumsInts[i] = stoi(A[i]);
     }
     
-   for(i = 0; i<arraySize; i++){
+    for(i = 0; i<arraySize; i++){
         Keys[i] = stoi(keyStrings[i]);
     }
     
@@ -122,11 +128,11 @@ int* extraction_method(string A[], int arraySize, int ProbeNum) {
     for(i = 0; i<arraySize; i++){
         //If there is nothing in the hash table at that key value, I set it equal to the SSNum at the same index the array of SS numbers
         if(HashTable[Keys[i]] == 0){
-           HashTable[Keys[i]] = SSNumsInts[i];
+            HashTable[Keys[i]] = SSNumsInts[i];
         }
         else{
             //Otherwise, I create a temp index called index of the key value at that position. While the value in the hash table at the index of index is empty.
-            //If not, it reassigns index's value using the quadratic probing formula and the probe number taken in the from the user. 
+            //If not, it reassigns index's value using the quadratic probing formula and the probe number taken in the from the user.
             //If that new index value still isn't empty, it increments the probe number and reassigns the index value using the same quadratic probing formula but a new probe number.
             //If keeps doing this until it finds a space that is not empty and then assigns the a SS number to that new index.
             
@@ -161,7 +167,7 @@ void writeOutFile(int A[], int size){
     ofstream readFile1;
     readFile1.open("hashed_socials.txt");
     
-    readFile1<< "Hash Table Madness Results: \n";
+    readFile1<< "Hash Table Results: \n";
     
     for(int i=0; i< size; i++){
         if(i != (size-1)) {
@@ -173,6 +179,7 @@ void writeOutFile(int A[], int size){
     }
     
 }
+
 
 
 
